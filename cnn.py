61c6f8f0d11a0de36ba59c9c67974aff480f8b06
vikram0888 <vikramjha753@gmail.com>
2022-12-20 05:44:26 +0530

    cnn

--- cnn.py
+++ cnn.py
@@ -49,7 +49,7 @@ test_set = test_datagen.flow_from_directory('cats_and_dogs_filtered/validation',
 
 model = classifier.fit_generator(training_set,
                          steps_per_epoch = 8000,
-                         epochs = 1,
+                         epochs = 1000,
                          validation_data = test_set,    
                          validation_steps = 2000)
 
